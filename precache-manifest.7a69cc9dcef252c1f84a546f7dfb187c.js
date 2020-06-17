cc3742f6e26a9b5b6b696b30fc915b355ee452e4
Leotiv-Vibs <sobaka.ty.my@gmail.com>
2020-06-17 17:44:17 +0300

    Updates

---
+++ precache-manifest.7a69cc9dcef252c1f84a546f7dfb187c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad1da89854ccde36f07fd6ede0b9802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d6fc65be4348e9ba1f",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9a8afab291aa01c518f3",
+    "url": "/static/css/main.0a657cf5.chunk.css"
+  },
+  {
+    "revision": "e6d6fc65be4348e9ba1f",
+    "url": "/static/js/2.e3ee8877.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.e3ee8877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8afab291aa01c518f3",
+    "url": "/static/js/main.ac1e5a18.chunk.js"
+  },
+  {
+    "revision": "ebe4aecc36a8efd46cb5",
+    "url": "/static/js/runtime-main.ab41d1ba.js"
+  }
+]);
