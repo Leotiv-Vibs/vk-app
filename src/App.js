cde08489d7bdeaea5f5e41128c1eb455137c0b24
Leotiv-Vibs <sobaka.ty.my@gmail.com>
2020-06-17 17:28:57 +0300

    vk_app_first

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import View from '@vkontakte/vkui/dist/components/View/View';
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+
+
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+import Intro from './panels/Intro';
+
+const ROUTES = {
+	HOME: 'home',
+	INTRO: 'intro',
+}
+
+const STORAGE_KEYS = {
+	STATUS : 'status',
+}
+
+
+const App = () => {
+	const [activePanel, setActivePanel] = useState(ROUTES.INTRO);
+	const [fetchedUser, setUser] = useState(null);
+	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	const [userSeeIntro, setUserSeeIntro] = useState(false)
+
+	useEffect(() => {
+		bridge.subscribe(({ detail: { type, data }}) => {
+			if (type === 'VKWebAppUpdateConfig') {
+				const schemeAttribute = document.createAttribute('scheme');
+				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+				document.body.attributes.setNamedItem(schemeAttribute);
+			}
+		});
+		async function fetchData() {
+			const user = await bridge.send('VKWebAppGetUserInfo');
+			const storageData = await bridge.send('VKWebAppStorageGet',{
+				keys: Object.values(STORAGE_KEYS)
+			});
+			console.log(storageData);
+			const data = {};
+			storageData.keys.forEach(({key,value}) =>{
+				try {
+					data[key] = value ? JSON.parse(value) :{};
+					switch (key) {
+						case STORAGE_KEYS.STATUS:
+							if (data[key].hasSeen){
+								setActivePanel(ROUTES.HOME);
+								setUserSeeIntro(false)
+							}
+							break;
+						default:
+							// statements_def
+							break;
+					}
+
+				}
+				catch(error){
+					console.log(error);
+				}
+			} );
+			setUser(user);
+			setPopout(null);
+		}
+		fetchData();
+	}, []);
+
+	const go = panel => {
+		setActivePanel(panel);
+	};
+	const viewIntro = async function (){
+		try{
+			await bridge.send ('VKWebAppStorageSet',{
+				key:STORAGE_KEYS.STATUS,
+				value: JSON.stringify({
+					hasSeen: false
+				})
+			});
+			go(ROUTES.HOME)
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	return (
+		<View activePanel={activePanel} popout={popout}>
+			<Home id={ROUTES.HOME} fetchedUser={fetchedUser} go={go} />
+			<Intro id={ROUTES.INTRO} fetchedUser={fetchedUser} go={viewIntro}  userSeeIntro={userSeeIntro}/>
+		</View>
+	);
+}
+
+export default App;
+
---
+++ src/panels/Intro.js
@@ -0,0 +1,52 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+
+
+
+
+
+
+import './Intro.css';
+
+
+
+const Intro = ({id,fetchedUser,userSeeIntro,go}) => {
+	return (
+		<Panel id={id} centered ={true}>
+		<PanelHeader>
+			Дайте денек
+		</PanelHeader>
+		{(!userSeeIntro && fetchedUser) && 
+		  <Fragment> 
+				<Group>
+					<Div className ='User' >
+						{fetchedUser.photo_200 && <Avatar src={fetchedUser.photo_200}> </Avatar>}
+						<h2>Привет, {fetchedUser.first_name}!</h2>
+						<h3>Здесь ты можешь заработать деньги своими знаниями отвечая на вопросы викторины. Для участия в викторине нужно закинуть 50 рублей. Ответив на три вопроса, вы получаете выигрыш в размере (количество участников*50)/(количество победителей)</h3>
+					</Div>
+				</Group>
+		  
+
+		  <FixedLayout vertical = 'bottom' >
+		  	<Div>
+		  		<Button mode='commerce' size='xl' onClick={go}>
+		  			Базара зира начальник
+		  		</Button>
+		  	</Div>
+		  </FixedLayout>
+		  </Fragment>
+		}
+	</Panel>)
+};
+
+
+
+export default Intro;
