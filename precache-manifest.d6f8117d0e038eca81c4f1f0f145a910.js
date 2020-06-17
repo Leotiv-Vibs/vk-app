37073eab775cbca956cc2706ca5c8aa47a4dfa11
Leotiv-Vibs <sobaka.ty.my@gmail.com>
2020-06-17 17:47:15 +0300

    Updates

---
+++ precache-manifest.d6f8117d0e038eca81c4f1f0f145a910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6a61d46b6f535ebbbae31c244064fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6d6fc65be4348e9ba1f",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9a8afab291aa01c518f3",
+    "url": "./static/css/main.0a657cf5.chunk.css"
+  },
+  {
+    "revision": "e6d6fc65be4348e9ba1f",
+    "url": "./static/js/2.e3ee8877.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.e3ee8877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8afab291aa01c518f3",
+    "url": "./static/js/main.ac1e5a18.chunk.js"
+  },
+  {
+    "revision": "8af75692b7655e2cedf6",
+    "url": "./static/js/runtime-main.7e9b59ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a69cc9dcef252c1f84a546f7dfb187c.js"
+  "./precache-manifest.d6f8117d0e038eca81c4f1f0f145a910.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
